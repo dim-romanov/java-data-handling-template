b4ff4786aff82885c6ef7df75ffea83796827158
Dmitrii Romanov <dromanovsar@gmail.com>
2020-04-15 17:14:44 +0400

    Completed class SimpleBigNumbersService

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -2,6 +2,7 @@ package com.epam.izh.rd.online.service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class SimpleBigNumbersService implements BigNumbersService {
 
@@ -13,7 +14,7 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigDecimal getPrecisionNumber(int a, int b, int range) {
-        return null;
+        return new BigDecimal(a).divide(new BigDecimal(b), range, RoundingMode.HALF_UP);
     }
 
     /**
@@ -24,6 +25,24 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+        int count = 0;
+        int simpleNumber = 2;
+        boolean isSimple = true;
+
+        while (count < range){
+            simpleNumber++;
+            for (int i = 2; i <= simpleNumber / 2; i++){
+                int simple = simpleNumber % i;
+                if (simple==0){
+                    isSimple = false;
+                    break;
+                }
+            }
+            if(isSimple){
+                count++;
+            }
+            isSimple=true;
+        }
+        return BigInteger.valueOf(simpleNumber);
     }
 }
